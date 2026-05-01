fa48d09722afdfc19b714ace79fbfece002678c8
torrezronan <torrezronan@gmail.com>
2026-05-01 20:45:39 -0300

    Empleado permanente basico y test de funcionamiento

--- src/test/Test_tp02.java
+++ src/test/Test_tp02.java
@@ -4,13 +4,16 @@ package test;
 import java.time.LocalDate;
 
 import ar.unq.tp02.Empleado;
+import ar.unq.tp02.EmpleadoPermanente;
 
 public class Test_tp02{
 
 	public static void main(String[] args) {
 		Empleado gustavo = new Empleado("ronan","colombia 1650","Soltero",LocalDate.of(1998, 12, 18));
+		
+		EmpleadoPermanente gustavoPermanente = new EmpleadoPermanente("ronan","colombia 1650","Soltero",LocalDate.of(1998, 12, 18),5,3);
 	
-		System.out.println(gustavo.edad());
+		System.out.println(gustavoPermanente.edad());
 	
 
 	}
