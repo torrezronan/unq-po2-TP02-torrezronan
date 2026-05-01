1aa956b87ef685ac5ee2c05374b43b3e50bca201
torrezronan <torrezronan@gmail.com>
2026-05-01 17:48:30 -0300

    Empresa V1

--- src/ar/unq/tp02/Empresa.java
+++ src/ar/unq/tp02/Empresa.java
@@ -1,7 +1,18 @@
 package ar.unq.tp02;
 
+import java.util.List;
+
 public class Empresa {
+	
 	private String nombreEmpresa;
 	private int cuit;
-
+	private List<Empleado> empleados;
+	
+	
+	public Empresa(String nombreEmpresa){
+		this.nombreEmpresa = nombreEmpresa;
+		
+	}
+	
 }
+
